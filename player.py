557b4ad4b118e71a00ffc764c8b2db5e43791f05
LilypadNL <lilyhoenig@gmail.com>
2025-11-09 17:11:41 -0500

    H: Shoot rate limit and destruction works.

--- player.py
+++ player.py
@@ -9,6 +9,7 @@ class Player(CircleShape):
     def __init__(self, x, y, PLAYER_RADIUS):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_timer = 0
 
     # in the player class
     def triangle(self):
@@ -42,8 +43,10 @@ class Player(CircleShape):
             self.move(dt*-1)
         if keys[pygame.K_SPACE]:
             self.shoot()
+        self.shot_timer -= dt
 
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
-        shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-        
+        if self.shot_timer <= 0:
+            shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+            shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            self.shot_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
