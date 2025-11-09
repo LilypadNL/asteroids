6029a5c03e943e91d31bf40042ebe8ecdae118fb
LilypadNL <lilyhoenig@gmail.com>
2025-11-09 15:52:49 -0500

    F: Collisions work, starting 4.3

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 import pygame
 from constants import *
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroidfield import AsteroidField
 from asteroid import Asteroid
+import sys
 
 def main():
+    #Initializing game
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+
+
     #Game clock
     game_clock = pygame.time.Clock()
     dt = 0
-    
+   
+
+    #Defining groups and membership
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (updatable, drawable, asteroids)
     AsteroidField.containers = (updatable)
+
 
     #Initiate Player object
     Player_sprite = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)   
@@ -37,6 +43,11 @@ def main():
                 return
         pygame.Surface.fill(screen, BLACK)
         updatable.update(dt)
+        for asteroid in asteroids:
+            if Player.collides_with(Player_sprite, asteroid) == True:
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         for sprite in drawable:
             sprite.draw(screen)
         pygame.display.flip()
